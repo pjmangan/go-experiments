e8155262bc255ca0bf3b65dee23dfb0df0b18291
Peter Mangan <peter.mangan@intel.com>
2024-08-26 14:34:26 +0100

    simple go curl

---
+++ curl.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+)
+
+func main() {
+    fmt.Println("Its kicking off..")
+
+    if len(os.Args) < 2 {
+        // If argument is not provided quit
+        log.Fatalln("url not provided")
+    }
+    url := os.Args[1] // URL
+
+    // cmd := exec.Command("curl", "-O", url)
+    cmd := exec.Command("curl", "-o", "output.txt", url)
+    cmd.Run()
+}
+
